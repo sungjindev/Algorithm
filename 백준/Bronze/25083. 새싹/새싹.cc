d4e5740102bc7c3f9ce57e50744f7402fc391eea
Logan <77658361+L-o-g-a-n@users.noreply.github.com>
2022-08-17 16:34:22 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+cout << "         ,r\'\"7" << '\n';
+cout << "r\`-_   ,\'  ,\/" << '\n';
+cout << " \\. \". L_r\'" << '\n';
+cout << "   \`~\\\/" << '\n';
+cout << "      \|" << '\n';
+cout << "      \|" << '\n';
+    
+    return 0;
+}

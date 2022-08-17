403de0da68ed1fd7e11ea9d2e885707ec34d49d2
Logan <77658361+L-o-g-a-n@users.noreply.github.com>
2022-08-17 16:32:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    
+    char c;
+    cin >> c;
+    cout << (int)c;
+    
+    return 0;
+}

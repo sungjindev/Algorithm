7263df03035b006e08ea61a140af4af115e961c5
Logan <77658361+L-o-g-a-n@users.noreply.github.com>
2022-08-17 16:29:29 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  int A,B,C;
+  cin >> A >> B >> C;
+
+  cout << (A+B)%C << endl;
+  cout << ((A%C) + (B%C))%C << endl;
+  cout << (A*B)%C << endl;
+  cout << ((A%C) * (B%C))%C << endl;
+
+  return 0;
+}

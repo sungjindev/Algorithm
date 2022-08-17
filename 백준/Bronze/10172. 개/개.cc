1a04369b455521687838cd0700b6d2fcab858b65
Logan <77658361+L-o-g-a-n@users.noreply.github.com>
2022-08-17 16:29:00 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+cout << "\|\\_\/\|" << '\n';
+cout << "\|q p\|   \/\}" << '\n';
+cout << "( 0 )\"\"\"\\" << '\n';
+cout << "\|\"\^\"\`    \|" << '\n';
+cout << "\|\|_\/\=\\\\__\|" << '\n';
+    return 0;
+}

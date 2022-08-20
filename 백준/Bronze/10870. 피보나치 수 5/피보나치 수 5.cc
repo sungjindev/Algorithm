6961615021058ca4de5115b262fc25fe273c5e13
Logan <77658361+L-o-g-a-n@users.noreply.github.com>
2022-08-21 00:11:26 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n) {
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    return fibo(n-1) + fibo(n-2);
+}
+
+int main(void) {
+    int n = 0;
+    cin >> n;
+    cout << fibo(n);
+    return 0;
+}

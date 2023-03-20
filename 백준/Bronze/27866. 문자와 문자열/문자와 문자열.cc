d0c3caab818d734342994b2174d069c651b4cbc2
Logan <77658361+xylogan@users.noreply.github.com>
2023-03-20 17:06:19 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    string str1;
+    int i;
+    cin >> str1 >> i;
+    cout << str1[i-1];
+    
+    return 0;
+}

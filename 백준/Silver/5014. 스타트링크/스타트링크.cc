000c16afc3a566999f53c7f27a57f2cc725bb3ac
Logan <77658361+xylogan@users.noreply.github.com>
2023-04-13 23:21:31 +0900

    [Silver I] Title: 스타트링크, Time: 36 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -0,0 +1,48 @@
+//최단 경로를 구해야하므로 BFS이다!
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define MAX 1000001
+
+int visited[MAX] = {0,};
+int depth[MAX] = {0,};
+
+int main(void) {
+    int f,s,g,u,d;
+    cin >> f >> s >> g >> u >> d;
+    queue<int> q1;
+    q1.push(s);
+    visited[s] = 1;
+    
+    
+    while(!q1.empty()) {
+        if(q1.front() == g) {
+            break;
+        } else {
+            if(q1.front()+u <= f) {
+                if(!visited[q1.front()+u]) {
+                    q1.push(q1.front()+u);
+                    visited[q1.front()+u] = 1;
+                    depth[q1.front()+u] = depth[q1.front()]+1;
+                }
+            }
+            if(q1.front()-d >= 1) {
+                if(!visited[q1.front()-d]) {
+                    q1.push(q1.front()-d);
+                    visited[q1.front()-d] = 1;
+                    depth[q1.front()-d] = depth[q1.front()]+1;
+                }
+            }
+        }
+        q1.pop();
+    }
+    
+    if(!q1.empty())
+        cout << depth[g];
+    else
+        cout << "use the stairs";
+    
+    return 0;
+}

56cf6fab83fdc38811ff4eab5af3d3dec2ce222f
Sungjin Park <77658361+sungjindev@users.noreply.github.com>
2023-12-15 23:28:36 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 3140 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int temp,n=0;
+    int current = 1;
+    cin >> n;
+    string str1="";
+    vector<int> v1;
+    stack<int> st1;
+
+    for(int i=0; i<n; ++i) {
+        cin >> temp;
+        v1.push_back(temp);
+    }
+
+    for(int i=0; i<n; ++i) {
+        if(st1.empty()) {
+            while(current <= v1[i]) {
+                st1.push(current++);
+                str1 += "+";
+            }
+            st1.pop();
+            str1 += "-";
+        } else if(st1.top() < v1[i]) {
+            while(current <= v1[i]) {
+                st1.push(current++);
+                str1 += "+";
+            }
+            st1.pop();
+            str1 += "-";
+        } else if(st1.top() == v1[i]) {
+            st1.pop();
+            str1 += "-";
+        } else if(st1.top() > v1[i]) {
+            str1 = "NO";
+            break;
+        }
+    }
+    
+    if(str1 == "NO") {
+        cout << str1;
+    } else {
+        for(auto v: str1)
+            cout << v << "\n";
+    }
+    
+    return 0;
+}

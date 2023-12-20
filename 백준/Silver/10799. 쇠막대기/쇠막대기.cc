7fe03afe5dff3e27d382950ec7a02a18808002db
Sungjin Park <77658361+sungjindev@users.noreply.github.com>
2023-12-20 17:27:24 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -7,28 +7,19 @@ int main(void) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int answer = 0;
     string str1;
-    stack<string> st1;
+    stack<char> st1;
     cin >> str1;
     
-    for(auto ch: str1) {
-        if(ch == '(') {
-            string temp;
-            temp += ch;
-            st1.push(temp);
-        } else if(ch == ')') {
-            if(st1.top() == "(") {
+    for(int i=0; i<str1.size(); ++i) {
+        if(str1[i] == '(')
+            st1.push(str1[i]);
+        else {
+            if(str1[i-1] == '(') {
                 st1.pop();
-                st1.push("1");
+                answer += st1.size();
             } else {
-                int count = 0;
-                while(st1.top() != "(") {
-                    count += stoi(st1.top());
-                    st1.pop();
-                }
                 st1.pop();
-                answer += (count+1);
-                
-                st1.push(to_string(count));
+                ++answer;
             }
         }
     }

102e6b358aeb7ca719e758489f9c6d43be88cb53
박성진 (Sungjin Park) <77658361+sungjindev@users.noreply.github.com>
2024-02-01 18:32:27 +0900

    [Gold III] Title: 다리 만들기, Time: 0 ms, Memory: 2236 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -18,6 +18,11 @@ using namespace std;
 //굉장히 rough하고 maxi하게 시간 복잡도를 계산해보면 최대 10000칸에 대해 서로 다른 칸들에 대한 조회 10000번씩 한다고하면,
 //10000 * 10000 = 100000000이다. 이는 시간 제한 2초 안에 끝낼 수 있는 연산량이라 생각된다.
 
+//사실 이렇게 짠 알고리즘은 결국 O(N^4)이고, N은 100이니까 100000000으로 시간 제한에 걸리지 않는 것이다.
+//하지만 이 알고리즘을 좀 더 리팩터링할 수 있는 방법이 있는데 그건 바로 모든 섬에서 동시에 BFS를 돌리는 것이다.
+//즉, 처음에 섬을 구분하기 위한 divideIsland 메서드는 그대로 사용하고 여기서 O(N^2)이 걸린다.
+//그 다음 섬들에 대해 모두 따로따로 BFS가 돌았던 것을 모두 동시에 BFS Queue에 넣어서 처리하면 전체 O(N^2)으로 확 줄게된다.
+
 #define X first
 #define Y second
 
@@ -29,6 +34,7 @@ int minimum = 999999999;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int visited[100][100];
+queue<pair<int, int>> q;
 
 void divideIsland(void) {
     for(int i=0; i<n; ++i) {
@@ -60,48 +66,24 @@ void divideIsland(void) {
 }
 
 void makeBridge(void) {
-    for(int i=0; i<n; ++i) {
-        for(int j=0; j<n; ++j) {
-            if(map[i][j] == 0) {
-                for(int k=0; k<4; ++k) {
-                    int nx = i + dx[k];
-                    int ny = j + dy[k];
-                    
-                    if(nx < 0 || nx >= n || ny < 0 || ny >= n || map[nx][ny] == 0)  //배열의 바운더리를 넘어가거나 섬이 아닌 경우 Skip
-                        continue;
-                    
-                    bool breakFlag = false;
-                    int currIslandNumber = map[nx][ny];
-                    fill(&visited[0][0], &visited[n-1][n], 0);  //visited 배열을 재활용하기 위한 초기화
-                    visited[i][j] = 0;
-                    queue<pair<int ,int>> q1;
-                    q1.push({i,j});
-                    
-                    while(!q1.empty() && !breakFlag) {
-                        auto curr = q1.front();
-                        q1.pop();
-                        
-                        for(int z=0; z<4; ++z) {
-                            int nnx = curr.X + dx[z];
-                            int nny = curr.Y + dy[z];
-                            int nnc = visited[curr.X][curr.Y] + 1;
-                            
-                            if(nnx < 0 || nnx >= n || nny < 0 || nny >= n || map[nnx][nny] == currIslandNumber || visited[nnx][nny]) //시작 때 존재한 인근 섬과 같은 섬이면 skip
-                                continue;
-                            
-                            if(map[nnx][nny] != 0) {
-                                minimum = min(minimum, nnc);
-                                breakFlag = true;
-                                break;
-                            }
-                            
-                            //0일 때는 이어서 BFS 탐색 진행 
-                            visited[nnx][nny] = nnc;
-                            q1.push({nnx, nny});
-                        }
-                    }
-                    
-                }
+    while(!q.empty()) {
+        auto curr = q.front();
+        q.pop();
+        
+        for(int i=0; i<4; ++i) {
+            int nx = curr.X + dx[i];
+            int ny = curr.Y + dy[i];
+            
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n || map[curr.X][curr.Y] == map[nx][ny]) {    //인접한 섬과 같은 섬일경우 skip
+                continue;
+            }
+            
+            if(map[nx][ny] == 0) {
+                map[nx][ny] = map[curr.X][curr.Y];
+                visited[nx][ny] = visited[curr.X][curr.Y] + 1;
+                q.push({nx, ny});
+            } else {    //이건 다른 섬이라는 것이다
+                minimum = min(minimum, visited[curr.X][curr.Y] + visited[nx][ny]);
             }
         }
     }
@@ -119,6 +101,16 @@ int main(void) {
     }
     
     divideIsland(); //섬마다 구분하여 map에 표시해주기 위한 함수
+    
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            if(map[i][j] != 0) {
+                visited[i][j] = 0;
+                q.push({i, j});
+            }
+        }
+    }
+    
     makeBridge();   //서로 다른 섬을 이어주는 다리를 만드는 과정
     
     cout << minimum;

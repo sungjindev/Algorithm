996f3aa4121f12ec4d5c2e8820b566c3c972a6b2
박성진 (Sungjin Park) <77658361+sungjindev@users.noreply.github.com>
2024-03-23 01:19:21 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,38 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int arr[3000000] = {0, };
+//이번에는 Dynamic Programming 문제를 풀어보려고 한다.
+//DP란 작은 단위로 문제를 쪼개서 Memorizing해두고 이를 활용해서 더 큰 문제를 점차적으로 해결해나가는 문제이다.
 
-void calc(int n);
+int d[1000001]; //DP를 위한 D 테이블
 
 int main(void) {
     int n=0;
     cin >> n;
+    d[1] = 0;
     
-    calc(n);
-
-    return 0;
-}
-
-void calc(int n) {
-    for(int i=1; i<n; i++) {
-        if(!arr[i+1]) {
-            arr[i+1] = arr[i]+1;
-        } else if(arr[i+1] > arr[i]+1) {
-            arr[i+1] = arr[i]+1;
-        }
-        
-        if(!arr[i*2]) {
-            arr[i*2] = arr[i]+1;
-        } else if(arr[i*2] > arr[i]+1) {
-            arr[i*2] = arr[i]+1;
-        }
-        
-        if(!arr[i*3]) {
-            arr[i*3] = arr[i]+1;
-        } else if(arr[i*3] > arr[i]+1) {
-            arr[i*3] = arr[i]+1;
-        }
+    for(int i=2; i<=n; ++i) {
+        d[i] = d[i-1] + 1;
+        if(i%2 == 0)
+            d[i] = min(d[i], d[i/2] + 1);   //여기 반드시 min으로 비교해줘야함! 이유는 d[i/2]보다 d[i-1]이 더 작을 수 있다!!!
+        if(i%3 == 0)
+            d[i] = min(d[i], d[i/3] + 1);
     }
-    cout << arr[n];
+    cout << d[n];
 }

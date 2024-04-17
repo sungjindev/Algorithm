b6087b1d64fce689d2c2e2cf857fdb7ad51260bd
박성진 (Sungjin Park) <77658361+sungjindev@users.noreply.github.com>
2024-04-17 18:20:08 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,39 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
+//이 문제는 쉽게 말하면 조합에 관한 문제이다.
+//특히 이 문제는 백트래킹을 통해 해결하면 쉽게 풀 수 있다.
+//아니면 next_permutation을 활용하여 조합을 구해 풀어도 된다.
+
+int n,s,cnt;
+int arr[20];
+
+void func(int curr, int total) {
+    if(curr == n) {
+        if(total == s)
+            ++cnt;
+        return;
+    }
+    
+    func(curr+1, total);
+    func(curr+1, total + arr[curr]);
+}
+
 int main(void) {
-    int n,s,temp,count=0;
-    vector<int> v1;
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n >> s;
-    
-    for(int i=0; i<n; i++) {
-        cin >> temp;
-        v1.push_back(temp);
+    for(int i=0; i<n; ++i) {
+        cin >> arr[i];
     }
     
-    sort(v1.begin(),v1.end());
-    
-    
-    for(int j=1; j<=n; j++) {
-        vector<int> com(n-j,0);
-        for(int z=0; z<j; z++)
-            com.push_back(1);
-    
-        do {
-            int sum = 0;
-            for(int u=0; u<com.size(); u++) {
-                if(com[u]==1) {
-                    sum += v1[u];
-                }
-            }
-            if(sum == s)
-                count++;
-        } while(next_permutation(com.begin(), com.end()));
-    }
-    
-    cout << count;
+    func(0,0);
+    if(s==0)    //크기가 양수인 부분 수열이므로 공수열(?)은 빼줘야 된다
+        cout << cnt-1;
+    else
+        cout << cnt;
     
     return 0;
 }

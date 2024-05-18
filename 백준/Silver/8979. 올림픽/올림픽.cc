d1d96aa037c0405343cc4fab9059320aee19a917
박성진 (Sungjin Park) <77658361+sungjindev@users.noreply.github.com>
2024-05-18 18:40:46 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2160 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -13,7 +13,7 @@ bool cmp(tuple<int,int,int,int> a, tuple<int,int,int,int> b) {
     if(get<0>(a) == get<0>(b)) {
         if(get<1>(a) == get<1>(b)) {
             if(get<2>(a) == get<2>(b)) {
-                return true;
+                return get<3>(a) > get<3>(b);
             }
             return get<2>(a) > get<2>(b);
         }
